c7114b36862745a44231f53c0024715d04659446
Nico Almiron <almiron.nicolas16@gmail.com>
2023-07-30 18:44:22 -0300

    se agrego un logo y se creo el titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { Container } from "react-bootstrap";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Titulo from "./component/Titulo";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Container>
+        <Titulo></Titulo>
+      </Container>
+    </>
+  );
 }
 
 export default App;
